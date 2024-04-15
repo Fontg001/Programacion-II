8f6b03fce3060f9fb0a34d4b9d1b551815050da9
Fontg001 <164901154+Fontg001@users.noreply.github.com>
2024-04-15 02:28:31 -0300

    Actividad N°5

---
+++ CuadranteDeUnPunto.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CuadranteDeUnPunto {
+    public static void main(String[] args) {
+        // Pedir al usuario que ingrese las coordenadas x e y
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Pedir al usuario que ingrese las coordenadas x e y
+            System.out.println("Ingrese la coordenada x del punto:");
+            int x = scanner.nextInt();
+            
+            System.out.println("Ingrese la coordenada y del punto:");
+            int y = scanner.nextInt();
+            
+            // Verificar si el punto se encuentra en el origen
+            if (x == 0 && y == 0) {
+                System.out.println("El punto se encuentra en el origen.");
+            } else if (x > 0 && y > 0) {
+                System.out.println("El punto se encuentra en el primer cuadrante.");
+            } else if (x < 0 && y > 0) {
+                System.out.println("El punto se encuentra en el segundo cuadrante.");
+            } else if (x < 0 && y < 0) {
+                System.out.println("El punto se encuentra en el tercer cuadrante.");
+            } else if (x > 0 && y < 0) {
+                System.out.println("El punto se encuentra en el cuarto cuadrante.");
+            } else {
+                System.out.println("El punto no se encuentra en ningún cuadrante.");
+            }
+        }
+    }
+}
